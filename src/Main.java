ff2eb872a68e6069ee0ba678e52f4242f59b38a1
Gabr12el <98735582+Gabr12el@users.noreply.github.com>
2022-03-19 10:18:25 +0400

    инициалкоммит

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        //Объявляете переменные для входных данных и параметров программы:
+        int tiketprice =100; // хранение стоимости 1билета
+        int bonusmile = 20; //хранения количества20р = 1миля
+        // Рассчитываете количество бонусных милль и выводим на экран
+        System.out.println("Количество бонусных миль:" + (tiketprice/bonusmile));
+    }
+}
