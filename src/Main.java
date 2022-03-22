c65cbe2d590a6922d31bef6acdf2b6e047c76587
Gabr12el <98735582+Gabr12el@users.noreply.github.com>
2022-03-22 18:00:45 +0400

    1правка

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         //Объявляете переменные для входных данных и параметров программы:
-        int tiketprice =100; // хранение стоимости 1билета
-        int bonusmile = 20; //хранения количества20р = 1миля
+        int tiketPrice = 100; // хранение стоимости 1 билета
+        int bonusMile = 20; //хранения количества 20р = 1миля
         // Рассчитываете количество бонусных милль и выводим на экран
-        System.out.println("Количество бонусных миль:" + (tiketprice/bonusmile));
+        System.out.println("Количество бонусных миль:" + (tiketPrice / bonusMile));
     }
 }
